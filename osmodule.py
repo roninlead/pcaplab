101d4d84208e4d974e37a3f412bf6fc9e6eb48b4
roninlead <roninlead@internet.ru>
2024-06-25 00:37:12 +0400

    PCAP 4.4.1.8 LAB: The os module

--- osmodule.py
+++ osmodule.py
@@ -1,5 +1,28 @@
 import os
 '''
+Scenario
+
+It goes without saying that operating systems allow you to search for files and directories. While studying this part of the course, you learned about the functions of the os module, which have everything you need to write a program that will search for directories in a given location.
+
+To make your task easier, we have prepared a test directory structure for you:
+
+Directory structure
+
+
+Your program should meet the following requirements:
+
+    Write a function or method called find that takes two arguments called path and dir. The path argument should accept a relative or absolute path to a directory where the search should start, while the dir argument should be the name of a directory that you want to find in the given path. Your program should display the absolute paths if it finds a directory with the given name.
+    The directory search should be done recursively. This means that the search should also include all subdirectories in the given path.
+
+Example input:
+path="./tree", dir="python"
+
+Example output:
+.../tree/python
+.../tree/cpp/other_courses/python
+.../tree/c/other_courses/python
+
+
 os.makedirs("./tree/c/other_courses/cpp")
 os.makedirs("./tree/c/other_courses/python")
 os.makedirs("./tree/cpp/other_courses/c")
@@ -22,4 +45,4 @@ def find(path,dir):
     except Exception as e:
         print(e)
 
-find("./tree/python", "python")
+find("./tree", "python")
